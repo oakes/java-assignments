ef0c963c48be52e82c0222cc382ac4648500a8c1
oakes <zsoakes@gmail.com>
2015-10-18 11:25:54 -0400

    Add Countries project

---
+++ projects/Countries/src/Countries.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Countries {
+    public static void main(String[] args) {
+
+    }
+
+    static String readFile(String fileName) {
+        File f = new File(fileName);
+        try {
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] fileContent = new char[fileSize];
+            fr.read(fileContent);
+            return new String(fileContent);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    static void writeFile(String fileName, String fileContent) {
+        File f = new File(fileName);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(fileContent);
+            fw.close();
+        } catch (Exception e) {
+
+        }
+    }
+}
