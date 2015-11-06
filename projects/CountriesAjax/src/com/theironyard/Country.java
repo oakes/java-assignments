aa59f392934a2b2114240af41c7ed55746fcbd08
oakes <zsoakes@gmail.com>
2015-11-05 22:36:42 -0500

    Add CountriesAjax project

---
+++ projects/CountriesAjax/src/com/theironyard/Country.java
@@ -0,0 +1,19 @@
+package com.theironyard;
+
+public class Country {
+    int id;
+    String name;
+    String abbrev;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAbbrev() {
+        return abbrev;
+    }
+}
