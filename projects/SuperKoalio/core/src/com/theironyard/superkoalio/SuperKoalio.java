30c1817fea9b1007b2e93e4fcab54caa73e4d96e
oakes <zsoakes@gmail.com>
2016-02-18 12:19:45 -0500

    Simplify game

--- projects/SuperKoalio/core/src/com/theironyard/superkoalio/SuperKoalio.java
+++ projects/SuperKoalio/core/src/com/theironyard/superkoalio/SuperKoalio.java
@@ -4,49 +4,30 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class SuperKoalio extends ApplicationAdapter {
     SpriteBatch batch;
-    TextureRegion stand;
-    TextureRegion jump;
+    TextureRegion stand, jump;
     Animation walk;
-    FitViewport viewport;
-    OrthographicCamera camera;
-    TiledMap map;
-    OrthogonalTiledMapRenderer renderer;
 
-    float x = 0;
-    float y = 0;
-    float xv = 0;
-    float yv = 0;
-    float time = 0;
-    boolean canJump = true;
+    static final int WIDTH = 18;
+    static final int HEIGHT = 26;
+    static final int DRAW_WIDTH = WIDTH*3;
+    static final int DRAW_HEIGHT = HEIGHT*3;
+    static final float MAX_VELOCITY = 500;
+    static final float MAX_JUMP_VELOCITY = 2000;
+    static final int GRAVITY = -50;
 
-    final float MAX_VELOCITY = 500;
-    final float MAX_JUMP_VELOCITY = 2000;
-    final int WIDTH = 18;
-    final int HEIGHT = 26;
-    final int DRAW_WIDTH = WIDTH*3;
-    final int DRAW_HEIGHT = HEIGHT*3;
-    final int GRAVITY = -50;
+    float x, y, xv, yv, time;
+    boolean canJump, faceRight = true;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
-        viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera = new OrthographicCamera();
-        map = new TmxMapLoader().load("level1.tmx");
-        renderer = new OrthogonalTiledMapRenderer(map, 3);
-
         Texture sheet = new Texture("koalio.png");
         TextureRegion[][] tiles = TextureRegion.split(sheet, WIDTH, HEIGHT);
         stand = tiles[0][0];
@@ -56,20 +37,36 @@ public class SuperKoalio extends ApplicationAdapter {
 
     @Override
     public void render () {
+        time += Gdx.graphics.getDeltaTime();
+
         move();
-        draw();
+
+        TextureRegion img;
+        if (y > 0) {
+            img = jump;
+        }
+        else if (xv != 0) {
+            img = walk.getKeyFrame(time, true);
+        }
+        else {
+            img = stand;
+        }
+
+        Gdx.gl.glClearColor(0.5f, 0.5f, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        if (faceRight) {
+            batch.draw(img, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+        }
+        else {
+            batch.draw(img, x + DRAW_WIDTH, y, DRAW_WIDTH * -1, DRAW_HEIGHT);
+        }
+        batch.end();
     }
 
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width, height);
-        camera.setToOrtho(false, width, height);
-    }
-
-    static float decelerate(float velocity) {
-        float deceleration = 0.8f;
-        velocity = velocity * deceleration;
-        if (Math.abs(velocity) < 0.5f) {
+    float decelerate(float velocity, float deceleration) {
+        velocity *= deceleration;
+        if (Math.abs(velocity) < 1) {
             velocity = 0;
         }
         return velocity;
@@ -85,53 +82,24 @@ public class SuperKoalio extends ApplicationAdapter {
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             xv = MAX_VELOCITY;
+            faceRight = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             xv = MAX_VELOCITY * -1;
+            faceRight = false;
         }
 
         yv += GRAVITY;
 
-        x += xv * Gdx.graphics.getDeltaTime();
         y += yv * Gdx.graphics.getDeltaTime();
+        x += xv * Gdx.graphics.getDeltaTime();
 
         if (y < 0) {
             y = 0;
             canJump = true;
         }
 
-        xv = decelerate(xv);
-        yv = decelerate(yv);
-    }
-
-    void draw() {
-        time += Gdx.graphics.getDeltaTime();
-
-        TextureRegion img;
-        if (y > 0) {
-            img = jump;
-        }
-        else if (Math.round(xv) != 0) {
-            img = walk.getKeyFrame(time, true);
-        }
-        else {
-            img = stand;
-        }
-
-        Gdx.gl.glClearColor(0.5f, 0.5f, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-        renderer.setView(camera);
-        renderer.render();
-
-        batch.begin();
-        if (xv >= 0) {
-            batch.draw(img, x, y, DRAW_WIDTH, DRAW_HEIGHT);
-        }
-        else {
-            batch.draw(img, x + DRAW_WIDTH, y, DRAW_WIDTH * -1, DRAW_HEIGHT);
-        }
-        batch.end();
+        yv = decelerate(yv, 0.9f);
+        xv = decelerate(xv, 0.8f);
     }
 }
